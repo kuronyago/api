53f891608cf8824c879d96c4c4d5b9686228ce70
kuronyago <yegor.pesterev@gmail.com>
2020-04-08 09:46:58 +0300

    rsocket v0.5.1

--- rsocket/src/client.rs
+++ rsocket/src/client.rs
@@ -1,23 +1,30 @@
-use rsocket_rust::prelude::*;
+use rsocket_rust::prelude::{ClientResponder, EchoRSocket, Payload, RSocket, RSocketFactory};
 use rsocket_rust_transport_websocket::WebsocketClientTransport;
 use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let cli = RSocketFactory::connect()
-        .acceptor(|| Box::new(EchoRSocket))
+    // let echo = Box::new(EchoRSocket);
+    let acceptor: ClientResponder = Box::new(|| Box::new(EchoRSocket));
+
+    let client = RSocketFactory::connect()
+        .acceptor(acceptor)
         .transport(WebsocketClientTransport::from("127.0.0.1:8080"))
         .setup(Payload::from("READY!"))
         .mime_type("text/plain", "text/plain")
         .start()
         .await
         .unwrap();
-    let req = Payload::builder()
+
+    let req: Payload = Payload::builder()
         .set_data_utf8("Hello World!")
         .set_metadata_utf8("Rust")
         .build();
-    let res = cli.request_response(req).await.unwrap();
+
+    let res = client.request_response(req).await.unwrap();
+
     println!("got: {:?}", res);
-    cli.close();
+
+    client.close();
 
     Ok(())
 }
--- rsocket/src/server.rs
+++ rsocket/src/server.rs
@@ -1,4 +1,5 @@
-use rsocket_rust::prelude::{EchoRSocket, RSocketFactory};
+use log::info;
+use rsocket_rust::prelude::{EchoRSocket, RSocketFactory, ServerResponder};
 use rsocket_rust_transport_websocket::WebsocketServerTransport;
 use std::error::Error;
 
@@ -6,13 +7,17 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let ws_transport: WebsocketServerTransport = WebsocketServerTransport::from("127.0.0.1:8080");
 
+    let responder: ServerResponder = Box::new(|setup, _socket| {
+        println!("accept setup: {:?}", setup);
+        Ok(Box::new(EchoRSocket))
+    });
+
+    let on_start = Box::new(|| info!("start"));
+
     RSocketFactory::receive()
         .transport(ws_transport)
-        .acceptor(|setup, _socket| {
-            println!("accept setup: {:?}", setup);
-            Ok(Box::new(EchoRSocket))
-        })
-        .on_start(|| println!("start"))
+        .acceptor(responder)
+        .on_start(on_start)
         .serve()
         .await?;
 
